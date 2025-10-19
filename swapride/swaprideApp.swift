2d52a72ba887de4ee9d163c818f97c11efffc867
Jurij Sacharov <jurij.sacharov@gmail.com>
2025-10-19 12:35:49 +0200

    Initial Commit

---
+++ swapride/swaprideApp.swift
@@ -0,0 +1,17 @@
+//
+//  swaprideApp.swift
+//  swapride
+//
+//  Created by Jurij Sacharov on 19.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct swaprideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
